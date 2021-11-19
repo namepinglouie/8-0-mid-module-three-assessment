cfd1cc867a14fa52156fae9d3d9044be90093c91
Name Ping Louie <namepinglouie@gmail.com>
2021-11-19 15:51:57 -0500

    pass checkout test

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ class App extends Component {
       subtotal: 0,
       tax: 0,
       total: 0,
+
+      firstName: "",
+      lastName: "",
+      email: "",
+      creditCard: "",
+      zipCode: "",
     }
   }
 
@@ -31,6 +37,29 @@ class App extends Component {
     })
   }
 
+  handleUserInput = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleSubmitForm = (e) => {
+    let {firstName, lastName, email, creditCard, zipCode, total} = this.state;
+    e.preventDefault();
+    if(firstName === "" || lastName === "" || email === "") {
+      alert("Input is not valid");
+    } 
+    if(creditCard.length < 16 || !creditCard.match(/^[0-9]+$/)) {
+      alert("Credit card number is not valid");
+    } 
+    if(zipCode.length < 5 || !zipCode.match(/^[0-9]+$/)) {
+      alert("Zip code is not valid");
+    }
+    if(firstName && lastName && email && creditCard && zipCode) {
+      alert(`Purchase complete! You will be charged ${formatAPrice(total)}`);
+    }
+  }
+
   displayTax = () => {
     let totalTax = this.calculateTax(this.state.subtotal);
     this.setState({
@@ -39,11 +68,11 @@ class App extends Component {
   }
 
   render() {
-    let {subtotal, tax, total} = this.state;
+    let {subtotal, tax, total, firstName, lastName, email, creditCard, zipCode} = this.state;
 
     let displayProduct = this.state.data.map((product) => {
       return (
-        <div class="product-display">
+        <div className="product-display">
           <h3>{product.name}</h3>
           <p>Price: {formatAPrice(product.price)}</p>
           <button type ="submit" onClick={()=>this.handleProductClick(product)}>Add To Cart</button>
@@ -72,26 +101,26 @@ class App extends Component {
           </section>
           <section className="checkout-form">
             <h2>Checkout</h2>
-            <form id="checkout">
+            <form onSubmit={this.handleSubmitForm} id="checkout">
               <label htmlFor="firstName">First Name</label>
               <br />
-              <input type="text" />
+              <input type="text" onInput={this.handleUserInput} value={firstName} name="firstName" id="firstName" />
               <br />
               <label htmlFor="lastName">Last Name</label>
               <br />
-              <input type="text" />
+              <input type="text" onInput={this.handleUserInput} value={lastName} name="lastName" id="lastName" />
               <br />
               <label htmlFor="email">Email</label>
               <br />
-              <input type="email" />
+              <input type="email" onInput={this.handleUserInput} value={email} name="email" id="email" />
               <br />
-              <label htmlFor="creditCard">Credit Card</label>
+              <label htmlFor="creditCard" >Credit Card</label>
               <br />
-              <input type="number" />
+              <input type="text" onInput={this.handleUserInput} value={creditCard} name="creditCard" id="creditCard" />
               <br />
               <label htmlFor="zipCode">Zip Code</label>
               <br />
-              <input type="number" />
+              <input type="text" onInput={this.handleUserInput} value={zipCode} name="zipCode" id="zipCode" />
               <br />
               <button type="submit">Buy Now</button>
             </form>
